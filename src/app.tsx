35ce90a8f89309b11a6b96f19f434b8c7a1a2e21
jonas-colas <jonas.colas@yaho.com>
2021-04-18 01:33:20 +0200

    react-refresh webpack

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import img from './computadora.jpg'
 import logo from './slack.svg'
+import {ClickCounter} from './ClickCounter'
 
 export const App = () => {
   return <>
-    <h1>React TypeScript Webpack - {process.env.NODE_ENV} - {process.env.name}</h1>
+    <h1>Edited React TypeScript Webpack - {process.env.NODE_ENV} - {process.env.name}</h1>
     <img src={img} alt="img" width="300" height="300" />
     <img src={logo} alt="logo" width="300" />
+    <ClickCounter />
   </>
 }
